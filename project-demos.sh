2e90acdc2c399963b3cd498b4fc48ac616eb725f
Randy Schneck <i@rasch.co>
2022-04-06 03:57:38 -0400

    ✨ Add demo for bf

--- project-demos.sh
+++ project-demos.sh
@@ -12,3 +12,9 @@ curl --silent --output site/fraktur/demo/index.html \
 
 curl --silent --output site/fraktur/demo/index.js \
   https://git.sr.ht/~rasch/fraktur/blob/main/index.js
+
+# bf
+mkdir -p site/bf/demo
+
+curl --silent --output site/bf/demo/index.html \
+  https://git.sr.ht/~rasch/bf/blob/main/examples/index.html
